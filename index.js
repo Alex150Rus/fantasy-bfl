90ffdb453a0ce64115d640b95094ff5bab56b218
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-14 00:49:50 +0300

    added port

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ app.get('/db', async (req, res) => {
     res.send("Error " + err);
   }
 })
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
+
