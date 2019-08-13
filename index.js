a4fc9800c3711ff237eab3b7f829e2d618a9dbdf
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-14 00:26:12 +0300

    ready for heroku

---
+++ index.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.get('/users', (request, response) => {
+  response.send('Hello user!');
+})
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
