60cd7a00d2f49acaa898dae83c6ad233863b8e95
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-15 00:46:29 +0300

    added delete query

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ app.get('/news', async (req, res) => {
   }
 })
 
+app.get('/newsdel', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query("DELETE FROM news;");
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
