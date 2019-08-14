8a7da239658fce2b88510fc6e28a82d47b2d8eca
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-15 00:24:28 +0300

    time

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
-    const result = await client.query("INSERT INTO news(news_date, title, text) VALUES (14-08-2019, 'Краснодар разгромил Порту', 'Краснодар одержал неожиданную победу в Португалии и вышел в следующий раунд ЛЧ');");
+    const result = await client.query("INSERT INTO news(news_date, title, text) VALUES (2019-08-14, 'Краснодар разгромил Порту', 'Краснодар одержал неожиданную победу в Португалии и вышел в следующий раунд ЛЧ');");
     const results = { 'results': (result) ? result.rows : null};
     res.send(results);
     client.release();
