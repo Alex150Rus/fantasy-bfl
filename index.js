1005b571e3a174edf18c3e4d9ca2a7bfdc8defa4
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-15 21:26:15 +0300

    adding teams

--- index.js
+++ index.js
@@ -38,6 +38,46 @@ app.get('/newsadd', async (req, res) => {
   }
 })
 
+app.get('/teamadd', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query(
+      `INSERT INTO teams(team) 
+      VALUES
+      ('Дормаш'),
+      ('Икарус'),
+      ('Милсон'),
+      ('Силовик'),
+      ('Металлург'),
+      ('Citizens'),
+      ('Old School'),
+      ('Русичи'),
+      ('Десна'),
+      ('Ротор'),
+      ('Янг Бойз'),
+      ('Рубин'),
+      ('ШПЛ'),
+      ('800 ударов'),
+      ('Легион'),
+      ('Жемчужина '),
+      ('Десна Выг'),
+      ('Беседь'),
+      ('Сокол'),
+      ('Теннисисты'),
+      ('Черная фурия'),
+      ('Без Будущего'),
+      ('Сокол'),
+      ('Good Game')
+      ;`);
+    const results = { 'results': (result) ? result.rows : null};
+    res.json(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.get('/news', async (req, res) => {
   try {
     const client = await pool.connect()
