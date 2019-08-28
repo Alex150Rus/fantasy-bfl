a9baa23d1c8f258c419a48f8e4a2f8380a5694b7
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-28 23:19:36 +0300

    sm change

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(process.env.ORG_URL);
+
 module.exports = {
   token: process.env.TOKEN,
   org_url: process.env.ORG_URL, 
   db_url: process.env.DATABASE_URL,
-}
+};
