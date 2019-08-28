b9facc3ae4fbc04c84bd55eb261ea046a2e7001d
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-28 22:31:33 +0300

    added/news/:id

--- routes/news.js
+++ routes/news.js
@@ -20,6 +20,20 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const client = await pool.connect()
+    const result = await client.query("SELECT * FROM news WHERE id = ($1);", [id]);
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 router.get('/newsadd', async (req, res) => {
   try {
     const client = await pool.connect()
