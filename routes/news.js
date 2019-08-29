4b6b6ad55d195f9a9c546279041eef34b838a3c8
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-08-29 23:12:20 +0300

    added result

--- routes/news.js
+++ routes/news.js
@@ -75,7 +75,7 @@ router.put('/:id', async (req, res) => {
     const result = await client.query("UPDATE news SET news_date=($1), title=($2), text=($3), imgFileName=($4) WHERE id = ($5);",
     [data.news_date, data.title, data.text, data.imgFileName,id]);
 
-    const results = { 'results': (result) ? result : null};
+    const results = { 'results': (result) ? result.rowCount : null};
     res.send(results);
     client.release();
   } catch (err) {
