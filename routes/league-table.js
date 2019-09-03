9e72bd35ce7ddb347df6805bb11a37e00677abed
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-03 23:20:22 +0300

    added crud for league_table

--- routes/league-table.js
+++ routes/league-table.js
@@ -11,8 +11,72 @@ const pool = new Pool({
 router.get('/', async (req, res) => {
   try {
     const client = await pool.connect()
-    const result = await client.query("SELECT team_id, team, games_played, wins, draws, looses, goales_scored, goales_missed, points FROM league_table lt INNER JOIN teams ON teams.id = lt.team_id ORDER BY points DESC;");
+    const result = await client.query("SELECT id, team_id, team, games_played, wins, draws, looses, goales_scored, goales_missed, points FROM league_table lt INNER JOIN teams ON teams.id = lt.team_id ORDER BY points DESC;");
     const results = { 'league table': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+router.post('/create', async (req, res) => {
+  try {
+    const data = { team_id : req.body.team_id, games_played : req.body.games_played, wins : req.body.wins, draws : req.body.draws,
+      looses : req.body.looses, goales_scored : req.body.goales_scored, goales_missed : req.body.goales_missed, points : req.body.points}
+
+    const client = await pool.connect()
+    const result = await client.query("INSERT INTO league_table(team_id, games_played, wins, draws, looses, goales_scored, goales_missed, points) values($1, $2, $3, $4, $5, $6, $7, $8)", 
+    [req.body.team_id, req.body.games_played, req.body.wins, req.body.draws, req.body.looses, req.body.goales_scored, req.body.goales_missed,  req.body.points]);
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+router.post('/delete', async (req, res) => {
+  try {
+    const id = req.body.id;
+    const client = await pool.connect()
+    const result = await client.query("DELETE FROM league_table WHERE id=($1)", [id]);
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const client = await pool.connect()
+    const result = await client.query("SELECT * FROM laegue_table WHERE id = ($1);", [id]);
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const result =[];
+  try {
+    const id = req.params.id;
+    const data = { team_id : req.body.team_id, games_played : req.body.games_played, wins : req.body.wins, draws : req.body.draws,
+      looses : req.body.looses, goales_scored : req.body.goales_scored, goales_missed : req.body.goales_missed, points : req.body.points }
+    const client = await pool.connect()
+    const result = await client.query("UPDATE league_table SET team_id = ($1), games_played = ($2), wins = ($3), draws = ($4), looses = ($5), goales_scored = ($6), goales_missed = ($7), points = ($8)) WHERE id = ($9);",
+    [req.body.team_id, req.body.games_played, req.body.wins, req.body.draws, req.body.looses, req.body.goales_scored, req.body.goales_missed,  req.body.points, req.params.id]);
+
+    const results = { 'results': (result) ? result.rowCount : null};
     res.send(results);
     client.release();
   } catch (err) {
