a1eb84c03c05b4fe3aaf57f612b42b2db6fea52b
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-10 22:19:35 +0300

    sm change

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ const results = require('./routes/results');
 const leagueTable = require('./routes/league-table');
 const cloudinary = require('./routes/cloudinary')
 
+const multer = require("multer");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const dotenv = require('dotenv');
+dotenv.config();
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret 
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "demo",
+  allowedFormats: ["jpg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+  });
+  const parser = multer({ storage: storage });
+
+app.get('/cloudinary', function(req, res) {
+  res.send('<form method="post" action="/" enctype="multipart/form-data">'
+    + '<p>Image: <input type="file" name="image"/></p>'
+    + '<p><input type="submit"></p>'
+    + '</form>');
+});
+
+router.post('/cloudinary', parser.single("image"), (req, res) => {
+ console.log(req.file); // to see what is returned to you
+});
+
 app.use(bodyParser.json());
 //обязательно ограничивать домены
 app.use(cors({origin: true}));
