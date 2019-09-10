7291a0c4c7412454e9a0da4801f361d9375d1829
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-10 22:42:31 +0300

    work

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/cloudinary', function(req, res) {
 });
 
 app.post('/cloudinary', parser.single("image"), (req, res) => {
- console.log(req.file); // to see what is returned to you
+ res.send(req.file); // to see what is returned to you
 });
 
 //обязательно ограничивать домены
