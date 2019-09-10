c027f5760b1f7bc3e505526dcf5faf7d8d6051f6
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-10 20:38:26 +0300

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/teams', teams);
 app.use('/news', news);
 app.use('/results', results);
 app.use('/league-table', leagueTable);
-express.createServer().use('/cloudinary', cloudinary);
+app.use('/cloudinary', cloudinary);
 
 
 let port = process.env.PORT;
