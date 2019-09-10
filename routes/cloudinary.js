78cb2a486876705f1b1e734421a88c99d5a765ef
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-10 20:49:17 +0300

    1

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-const cloudinary = require('cloudinary');
+var cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv');
+dotenv.config();
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret 
+});
 
 // var app = express.createServer(express.logger());
 
