5aa058c7e9a8391f7519e9098be94e7e4757258e
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-09-10 21:45:50 +0300

    works

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-
-var cloudinary = require('cloudinary').v2;
+const multer = require("multer");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const cloudinary = require('cloudinary');
 const dotenv = require('dotenv');
 dotenv.config();
 cloudinary.config({ 
@@ -11,9 +11,15 @@ cloudinary.config({
   api_secret: process.env.api_secret 
 });
 
-// var app = express.createServer(express.logger());
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "demo",
+  allowedFormats: ["jpg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+  });
+  const parser = multer({ storage: storage });
 
-router.get('/', function(req, res) {
+router.get('/cloudinary', function(req, res) {
   res.send('<form method="post" enctype="multipart/form-data">'
     + '<p>Public ID: <input type="text" name="title"/></p>'
     + '<p>Image: <input type="file" name="image"/></p>'
@@ -21,20 +27,9 @@ router.get('/', function(req, res) {
     + '</form>');
 });
 
-router.post('/', function(req, res, next) {
-  stream = cloudinary.uploader.upload_stream(function(result) {
-    console.log(result);
-    res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
-             cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
-  }, { public_id: req.body.title } );
-  fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+router.post('/cloudinary', parser.single("image"), (req, res) => {
+  res.send(req.file) // to see what is returned to you
+  // 
 });
-
-// if (!module.parent) {
-//   var port = process.env.PORT || 5000;
-//   app.listen(port, function() {
-//     console.log("Listening on " + port);
-//   });
-// }
 
 module.exports = router;
